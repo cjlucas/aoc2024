c75515ddf1835aac5318ce08985179c302ed0cd8
Chris Lucas <chris@cjl.io>
2024-12-01 17:06:32 -0500

    part2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -20,8 +20,32 @@ fn part1(input: &str) -> u64 {
         .sum()
 }
 
+fn part2(input: &str) -> u64 {
+    let mut first = Vec::new();
+    let mut second = Vec::new();
+
+    for line in input.lines() {
+        let mut split = line.split_whitespace();
+        first.push(split.next().unwrap().parse::<u64>().unwrap());
+        second.push(split.next().unwrap().parse::<u64>().unwrap());
+    }
+
+    let mut sum = 0;
+
+    for i in first {
+        for j in &second {
+            if i == *j {
+                sum += i;
+            }
+        }
+    }
+
+    sum
+}
+
 fn main() {
     println!("part1: {}", part1(INPUT));
+    println!("part2: {}", part2(INPUT));
 }
 
 #[cfg(test)]
@@ -33,5 +57,12 @@ mod tests {
         let result = part1(include_str!("../../inputs/day01_example.txt"));
 
         assert_eq!(11, result);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = part2(include_str!("../../inputs/day01_example.txt"));
+
+        assert_eq!(31, result);
     }
 }
