4be920eda59ec84db1a2c2b897be6e28085d039e
Chris Lucas <chris@cjl.io>
2024-12-23 20:55:52 -0500

    --wip-- [skip ci]

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,44 +1,38 @@
 use regex::Regex;
 
-const INPUT: &str = include_str!("../../inputs/day03.txt");
+const INPUT: &str = include_str!("../../inputs/day04.txt");
+
+#[derive(Debug)]
+struct Grid {
+    rows: Vec<Vec<char>>,
+}
+
+impl Grid {
+    fn at(&self, row: usize, col: usize) -> Option<char> {
+        if row > self.rows.len() {
+            return None;
+        }
+
+        if col > self.rows[0].len() {
+            return None;
+        }
+
+        Some(self.rows[row][col])
+    }
+}
+
+fn parse_input(input: &str) -> Grid {
+    let rows = input.lines().map(|line| line.chars().collect()).collect();
+
+    Grid { rows }
+}
 
 fn part1(input: &str) -> u64 {
-    let re = Regex::new(r"mul\((\d+)\,(\d+)\)").unwrap();
-
-    let mut sum = 0;
-
-    for (_, [a, b]) in re.captures_iter(input).map(|c| c.extract()) {
-        sum += a.parse::<u64>().unwrap() * b.parse::<u64>().unwrap();
-    }
-
-    sum
+    let grid = parse_input(input);
 }
 
 fn part2(input: &str) -> u64 {
-    let mut enabled = true;
-    let re = Regex::new(r"(mul\(\d+,\d+\)|do\(\)|don\'t\(\))").unwrap();
-
-    let mut sum = 0;
-
-    for m in re.find_iter(input) {
-        match m.as_str() {
-            "do()" => {
-                enabled = true;
-            }
-            "don't()" => {
-                enabled = false;
-            }
-            s => {
-                if enabled {
-                    let re = Regex::new(r"mul\((\d+)\,(\d+)\)").unwrap();
-                    let cap = re.captures(s).unwrap();
-                    sum += cap[1].parse::<u64>().unwrap() * cap[2].parse::<u64>().unwrap();
-                }
-            }
-        }
-    }
-
-    sum
+    0
 }
 
 fn main() {
@@ -52,29 +46,29 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
-        let result = part1(include_str!("../../inputs/day03_example.txt"));
+        let result = part1(include_str!("../../inputs/day04_example.txt"));
 
-        assert_eq!(161, result);
+        assert_eq!(18, result);
     }
 
-    #[test]
-    fn test_part1() {
-        let result = part1(include_str!("../../inputs/day03.txt"));
+    // #[test]
+    // fn test_part1() {
+    //     let result = part1(include_str!("../../inputs/day04.txt"));
 
-        assert_eq!(188741603, result);
-    }
+    //     assert_eq!(0, result);
+    // }
 
-    #[test]
-    fn test_part2_example() {
-        let result = part2(include_str!("../../inputs/day03p2_example.txt"));
+    // #[test]
+    // fn test_part2_example() {
+    //     let result = part2(include_str!("../../inputs/day04p2_example.txt"));
 
-        assert_eq!(48, result);
-    }
+    //     assert_eq!(0, result);
+    // }
 
-    #[test]
-    fn test_part2() {
-        let result = part2(include_str!("../../inputs/day03.txt"));
+    // #[test]
+    // fn test_part2() {
+    //     let result = part2(include_str!("../../inputs/day04.txt"));
 
-        assert_eq!(67269798, result);
-    }
+    //     assert_eq!(0, result);
+    // }
 }
